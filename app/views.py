e71fdf19185d28de6e9b07fb4056a5a3d5080e9e
dhruba225 <dhru225@gmail.com>
2023-09-14 23:16:05 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render
+
+from app.models import *
+
+# Create your views here.
+
+def insert_webpage(request):
+    LTO=Topic.objects.all()
+    d={'LTO':LTO}
+    if request.method=='POST':
+        tn=request.POST['tn']
+        na=request.POST['na']
+        ur=request.POST['ur']
+        TO=Topic.objects.get(topic_name=tn)
+        WO=Webpage.objects.get_or_create(topic_name=TO,name=na,url=ur)[0]
+        WO.save()
+        QSWO=Webpage.objects.all()
+        d1={'QSWO':QSWO}
+        return render(request,'display_webpage.html',d1)
+    return render(request,'insert_webpage.html',d)
+
+
+def select_and_display(request):
+    LTO=Topic.objects.all()
+    d={'LTO':LTO}
+    if request.method=='POST':
+        tnlist=request.POST.getlist('tn')
+
+        QSWO=Webpage.objects.none()
+        for tn in tnlist:
+            QSWO=QSWO|Webpage.objects.filter(topic_name=tn)
+        d1={'QSWO':QSWO}
+        return render(request,'display_webpage.html',d1)
+    return render(request,'select_and_display.html',d)
+
+def checkbox(request):
+    LTO=Topic.objects.all()
+    d={'LTO':LTO}  
+    return render(request,'checkbox.html',d)
+
+# def delete_webpage(request):
+#     LTO=Topic.objects.all()
+#     d={'LTO':LTO}
+#     if request.method=='POST':
+#         tnlist=request.POST.getlist('topic')
+#         for to in tnlist:
+#             k=Topic.objects.get(topic_name=to)
+#             Webpage.objects.filter(topic_name=k)
+#         SCO=Webpage.objects.all()
+#         d1={'SCO':SCO}
+#         return render(request,'display_webpage.html',d1)
+#     return render(request,'select_and_display.html',d)
+
